7a73a811a672f2af89abc4b46ec695a3e23ddcd5
FrankPan <a1687193@gmail.com>
2021-02-15 21:30:22 +0800

    update ign

---
+++ PA2/b07901021_pa2/src/MaxPlanarSubset.h
@@ -0,0 +1,31 @@
+// **************************************************************************
+//  File       [MaxPlanarSubset.h]
+//  Author     [b07901021 Shih-Hsuan Pan]
+//  Synopsis   [The header file for the ChordSet, Node class]
+//  Modify     [2020/11/8 Shih-Hsuan Pan]
+// **************************************************************************
+
+#ifndef _MAXPLANARSUBSET_H
+#define _MAXPLANARSUBSET_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class ChordSet{
+    friend class Node;
+    public:
+        ChordSet();
+        ChordSet(int);
+        bool connection_assign(int, int);
+        int find_connection(int);
+        bool chord_existence(int, int);
+        void test();
+    private:
+        vector<int> data;
+
+};
+int MPS(int, int, ChordSet*, int**);
+void traverse(int, int, ChordSet*, fstream &, int**);
+#endif
